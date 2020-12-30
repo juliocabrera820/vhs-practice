d26a2144a540d61f31c729dba4807b33f27baec1
julio cabrera <juliocabrera820@gmail.com>
2020-12-30 16:14:44 -0600

    test: request

---
+++ spec/requests/clients_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Clients', type: :request do
+  describe 'GET /clients' do
+    it 'returns clients' do
+      get '/api/v1/clients'
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(0)
+    end
+  end
+  describe 'GET /clients' do
+    it 'returns all clients' do
+      FactoryBot.create_list(:random_client, 3)
+      get '/api/v1/clients'
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
+  end
+  describe 'POST /clients' do
+    it 'returns a new client' do
+      post '/api/v1/clients', params: { name: 'laura', home_address: 'c 55' }
+      expect(response).to have_http_status(:created)
+      expect(JSON.parse(response.body)).to eq(
+        { 'id' => 4,
+          'name' => 'laura',
+          'home_address' => 'c 55' }
+      )
+    end
+  end
+  describe 'POST /clients' do
+    it 'returns an unprocessable entity status' do
+      post '/api/v1/clients', params: { name: 'john', home_address: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+  describe 'GET /clients/5' do
+    it 'returns a client with id=5' do
+      FactoryBot.create(:client)
+      get '/api/v1/clients/5'
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to eq(
+        { 'id' => 5,
+          'name' => 'john',
+          'home_address' => 'c 21 #44' }
+      )
+    end
+  end
+  describe 'DELETE /api/v1/clients' do
+    let(:client) { FactoryBot.create(:random_client) }
+    it 'returns no content status' do
+      delete "/api/v1/clients/#{client.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
