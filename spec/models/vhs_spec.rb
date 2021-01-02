62a05ad9e59405c18a141e0d0b33bffda7951b56
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 15:58:17 -0600

    test: add have_many validation

--- spec/models/vhs_spec.rb
+++ spec/models/vhs_spec.rb
@@ -6,4 +6,7 @@ RSpec.describe Vhs, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:serial_number) }
   end
+  describe 'associations' do
+    it { should have_many(:rentals) }
+  end
 end
