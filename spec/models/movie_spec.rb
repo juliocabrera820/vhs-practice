57b345140cab6d402bc2483f62e10706468c8124
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 14:12:34 -0600

    test: group by validation test

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:year) }
-  it { should validate_presence_of(:length) }
-  it { should validate_presence_of(:director) }
-  it { should validate_presence_of(:description) }
-  it { should validate_numericality_of(:year) }
-  it { should validate_numericality_of(:length) }
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:year) }
+    it { should validate_presence_of(:length) }
+    it { should validate_presence_of(:director) }
+    it { should validate_presence_of(:description) }
+    it { should validate_numericality_of(:year) }
+    it { should validate_numericality_of(:length) }
+  end
 end
