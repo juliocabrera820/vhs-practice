b016a8b521cf76beb8be867017e6cc58f0dbd5e9
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 14:09:17 -0600

    feat: add genre routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :clients
       resources :movies
+      resources :genres
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
