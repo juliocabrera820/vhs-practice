3754ee4758fcc00d1630f23a6e06ac538d8522d1
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 15:54:58 -0600

    chore: add nested and rental routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  concern :rentable do
+    resources :rentals
+  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :clients
+      resources :clients, concerns: :rentable
       resources :movies
       resources :genres
-      resources :vhs
+      resources :vhs, concerns: :rentable
+      resources :rentals
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
