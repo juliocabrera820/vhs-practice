3946f4a817480978b70b1c02b1ecb43766433cfd
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 14:38:56 -0600

    test: model

---
+++ spec/models/vhs_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Vhs, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:serial_number) }
+  end
+end
