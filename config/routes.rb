3f00388515b8a15e6d1ebbfa871a6615c41b77a4
julio cabrera <juliocabrera820@gmail.com>
2021-01-02 16:48:10 -0600

    feat: add nested and movie genre routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   concern :rentable do
     resources :rentals
   end
+  concern :generable do
+    resources :movie_genres
+  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :clients, concerns: :rentable
-      resources :movies
-      resources :genres
       resources :vhs, concerns: :rentable
       resources :rentals
+      resources :movies, concerns: :generable
+      resources :genres, concerns: :generable
+      resources :movie_genres
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
