7f706c30ad713fa2e6469baab882a0f3d82430bb
julio cabrera <juliocabrera820@gmail.com>
2021-03-06 20:17:23 -0600

    refactor: use actions instead of model methods

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -3,44 +3,44 @@
 module Api
   module V1
     class ClientsController < ApplicationController
-      before_action :set_client, only: %i[show update destroy]
-
       def index
-        @clients = Client.all
+        @clients = ListClientsAction.new.perform.clients
         render json: @clients, status: :ok
       end
 
       def show
+        @client = ShowClientAction.new.perform(params[:id].to_i).client
         render json: @client, status: :ok
       end
 
       def create
-        @client = Client.new(client_params)
-        if @client.save!
-          render json: @client, status: :created
-        else
-          render json: @client.errors, status: :unprocessable_entity
+        @client_input = ClientInput.new(client_params)
+        CreateClientAction.new.perform(@client_input)
+                          .and_then do |client:|
+          render json: client, status: :created
+        end
+                          .or_else do |errors|
+          render json: errors, status: :unprocessable_entity
         end
       end
 
       def destroy
-        @client.destroy!
+        DeleteClientAction.new.perform(params[:id])
         head :no_content
       end
 
       def update
-        if @client.update(client_params)
-          render json: @client, status: :ok
-        else
-          render json: @client.errors, status: :unprocessable_entity
+        @client_input = ClientInput.new(client_params)
+        UpdateClientAction.new.perform(params[:id], @client_input)
+                          .and_then do |client:|
+          render json: client, status: :created
+        end
+                          .or_else do |errors|
+          render json: errors, status: :unprocessable_entity
         end
       end
 
       private
-
-      def set_client
-        @client = Client.find(params[:id])
-      end
 
       def client_params
         params.permit(:name, :home_address)
