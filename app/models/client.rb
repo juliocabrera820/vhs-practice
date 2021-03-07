f7a5943ef9a461a72873e1adafd2455f6079d106
julio cabrera <juliocabrera820@gmail.com>
2021-03-06 20:16:06 -0600

    feat: add client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-class Client < ApplicationRecord
-  include Rentable
-  validates :name, :home_address, presence: true
+class Client
+  attr_reader :id, :name, :home_address, :created_at, :updated_at
+
+  def initialize(id:, name:, home_address:, created_at:, updated_at:)
+    @id = id
+    @name = name
+    @home_address = home_address
+    @created_at = created_at
+    @updated_at = updated_at
+  end
 end
