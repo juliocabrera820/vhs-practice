f192e9c98503250089d11838833ed08e8fefa0d8
julio cabrera <juliocabrera820@gmail.com>
2021-03-09 15:19:08 -0600

    fix: set new path

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,9 @@ module VhsPractice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.autoload_paths += Dir[Rails.root.join('app', 'actions', 'client')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'actions', 'clients')]
     config.autoload_paths += Dir[Rails.root.join('app', 'inputs')]
+    config.autoload_paths += Dir[Rails.root.join('lib')]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
