2e80c46d76a45f31514ef3d33b1bb70f57a62f1f
julio cabrera <juliocabrera820@gmail.com>
2021-03-09 15:03:00 -0600

    feat: add base action

---
+++ app/actions/base_action.rb
@@ -0,0 +1,17 @@
+class BaseAction
+  class << self
+    attr_writer :result_class
+
+    def result_class
+      @result_class ||= ActionResult.new
+    end
+
+    def result(*args)
+      @result_class = ActionResult.new(*args)
+    end
+  end
+
+  def result
+    self.class.result_class
+  end
+end
