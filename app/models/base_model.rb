8fe974c08b3da862c655dd29417a611e330e908c
julio cabrera <juliocabrera820@gmail.com>
2021-03-09 15:10:22 -0600

    refactor: extract code to main class

---
+++ app/models/base_model.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class BaseModel < ImmutableObject 
+  attribute :id
+  attribute :created_at
+  attribute :updated_at
+
+  def initialize(**args)
+    self.class.attribute_names.each { |key| args.fetch(key) }
+
+    super
+  end
+
+  def to_param
+    id.to_s
+  end
+end
